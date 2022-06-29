d62a75a7e73c8a0690fcd21caad46597b65daaba
Codekacode <karem.quiroz.m@gmail.com>
2022-06-28 22:37:10 -0500

    Add useNavigation to push a new user to hom

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { auth } from "./firebase";
 import "./Login.css"
 
 function Login() {
+
+    const history = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -17,6 +19,9 @@ function Login() {
             .createUserWithEmailAndPassword(email, password)
             .then((auth) => {
                 console.log(auth);
+                if(auth) {
+                    history('/')
+                }
             })
             .catch((error) => alert(error.message))
     }
