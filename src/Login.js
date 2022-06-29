d7bb85b0233d9d1c9eadb48a43e0c7654258443a
Codekacode <karem.quiroz.m@gmail.com>
2022-06-28 22:04:45 -0500

    ~ Config firebase

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css"
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+    }
+
+    const register = e => {
+        e.preventDefault();
+    }
+
     return (
         <div className="login">
             <Link to="/">
@@ -14,16 +26,16 @@ function Login() {
                 <h1>Sign In</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type="email" />
+                    <input type="email" onChange={email} value={e => setEmail(e.target.value)} />
                     <h5>Password</h5>
-                    <input type="password" />
-                    <button className="login__signInButton">Sign In</button>
+                    <input type="password" onChange={password} value={e => setPassword(e.target.value)} />
+                    <button onClick={signIn} className="login__signInButton">Sign In</button>
                 </form>
                  <p>
                     By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
                     see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
                 </p>
-                <button className='login__registerButton'>Create your Amazon Account</button>
+                <button onClick={register} className='login__registerButton'>Create your Amazon Account</button>
             </div>
         </div>
     )
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
+import firebase from "firebase";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,5 +19,11 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+
+export { db, auth };
